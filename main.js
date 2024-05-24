a358c3703b28ab9f84bc9cd9de1497644c0c1edc
Aanya <85621509+ag-miroh7221@users.noreply.github.com>
2024-05-24 19:43:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+word = document.getElementById("word").value;
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(600, 450);
+
+    canvas = createCanvas(600, 450);
+    canvas.position(700, 160);
+    poseNet = ml5.poseNet (video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is intialised!');
+}
+
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
+    }
+}                                                                
+
+
+function draw() {
+    background ('#969A97');
+    text(word , leftWristX , rightWristX);
+    textSize(noseX , noseY , difference);
+    fill('#F90093');
+    document.getElementById("font_side").innerHTML = "Width And Height of the word = " + difference + "px";
+}
+   
+
+//function draw() {
+  //  background ('#969A97');
+ //   document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = " + difference + "px";
+   // fill('#F90093');
+   // stroke('#F90093');
+  //  square(noseX, noseY, difference)//
